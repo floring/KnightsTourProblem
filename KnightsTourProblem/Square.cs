a93c1672b96b0ac449926351024df84688577515
floring <floring@users.noreply.github.com>
2015-08-18 15:56:31 +0300

    Create Square class.

---
+++ KnightsTourProblem/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsTourProblem
+{
+    /// <summary>
+    /// Represents chess square entity.
+    /// </summary>
+    public class Square
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public bool HasVisited { get; set; }
+
+    }
+}

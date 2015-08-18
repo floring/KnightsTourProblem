5fac58773f56b5a4b4c06e1ceef8712b319b8743
floring <floring@users.noreply.github.com>
2015-08-18 16:15:30 +0300

    Create Board class.

---
+++ KnightsTourProblem/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightsTourProblem
+{
+    /// <summary>
+    /// Represents chessboard entity.
+    /// Each cell of this board is Squre class.
+    /// </summary>
+    class Board
+    {
+        private int _x;
+        private int _y;
+
+        public Board(int size)
+        {
+            _x = size;
+            _y = size;
+        }
+    }
+}

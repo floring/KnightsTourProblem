d6ca13fece2daa7c16b36a062b5dd6ff42643288
floring <floring@users.noreply.github.com>
2015-08-19 12:42:00 +0300

    Implement obtaining moves from current square in KnightMove class.

--- KnightsTourProblem/KnightMove.cs
+++ KnightsTourProblem/KnightMove.cs
@@ -20,19 +20,73 @@ namespace KnightsTourProblem
             var movesList = new List<Square>();
 
             var move = HorizontalLeftUp(square);
-            if (move != null) movesList.Add(move);
+            if (IsSquareValid(move)) movesList.Add(move);
+
+            move = HorizontalLeftDown(square);
+            if (IsSquareValid(move)) movesList.Add(move);
+
+            move = HorizontalRightUp(square);
+            if (IsSquareValid(move)) movesList.Add(move);
+
+            move = HorizontalRightDown(square);
+            if (IsSquareValid(move)) movesList.Add(move);
+
+            move = VerticalUpLeft(square);
+            if (IsSquareValid(move)) movesList.Add(move);
+
+            move = VerticalUpRight(square);
+            if (IsSquareValid(move)) movesList.Add(move);
+
+            move = VerticalDownLeft(square);
+            if (IsSquareValid(move)) movesList.Add(move);
+
+            move = VerticalDownRight(square);
+            if (IsSquareValid(move)) movesList.Add(move);
 
             return movesList;
         }
 
         private Square HorizontalLeftUp(Square square)
         {
-            var move = new Square(square.X - 1, square.Y - 2);
-            return (IsSquareValid(move)) ? move : null;
+            return new Square(square.X - 1, square.Y - 2);
         }
 
+        private Square HorizontalLeftDown(Square square)
+        {
+            return new Square(square.X + 1, square.Y - 2);
+        }
 
-        public bool IsSquareValid(Square square)
+        private Square HorizontalRightUp(Square square)
+        {
+            return new Square(square.X - 1, square.Y + 2);
+        }
+
+        private Square HorizontalRightDown(Square square)
+        {
+            return new Square(square.X + 1, square.Y + 2);
+        }
+
+        private Square VerticalUpLeft(Square square)
+        {
+            return new Square(square.X - 2, square.Y - 1);
+        }
+
+        private Square VerticalUpRight(Square square)
+        {
+            return new Square(square.X - 2, square.Y + 1);
+        }
+
+        private Square VerticalDownLeft(Square square)
+        {
+            return new Square(square.X + 2, square.Y - 1);
+        }
+
+        private Square VerticalDownRight(Square square)
+        {
+            return new Square(square.X + 2, square.Y + 1);
+        }
+
+        private bool IsSquareValid(Square square)
         {
             return (square.X < _board.RowSize && square.Y < _board.RowSize);
         }
